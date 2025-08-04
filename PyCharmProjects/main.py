709f3df427aae57bcd3b39bdf635a77d036b97b6
Lopez <Jaime.Lopez@VPGSensors.com>
2025-08-04 10:59:16 -0700

    User input practice

--- PyCharmProjects/main.py
+++ PyCharmProjects/main.py
@@ -1,9 +1,25 @@
 # Shopping cart program using an item, price, and quantity
 
-item = input("What item would you like to buy?: ")
-price = float(input("What is the price: "))
-quantity = int(input("How many would you like?: "))
+# item = input("What item would you like to buy?: ")
+# price = float(input("What is the price: "))
+# quantity = int(input("How many would you like?: "))
+#
+# total = price * quantity
+#
+# print(f"You have bought {quantity} {item}'s at {price} each. Your total is ${total}")
 
-total = price * quantity
 
-print(f"You have bought {quantity} {item}'s at {price} each. Your total is ${total}")
+# Madlibs game
+# Word game where you create a story
+# by filling in blanks with random words
+
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+adjective2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb ending with 'ing' : ")
+adjective3 = input("Enter an adjective (description): ")
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}.")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}.")
