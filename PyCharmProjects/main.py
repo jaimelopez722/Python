ff9860b1fcc47b4354778ffce8e1170977ee0ee5
Lopez <Jaime.Lopez@VPGSensors.com>
2025-08-04 13:58:41 -0700

    Arithmetic problems. Shows how to use math libraries

--- PyCharmProjects/main.py
+++ PyCharmProjects/main.py
@@ -1,37 +1,25 @@
 import math
 
-# arithmetic
+# 1. calculate circumference of a circle: C = 2*pi*radius
+# 2. calculate the area of a circle: A = pi * r^2
+# 3. Find hypotenuse of a right triangle: c = sqrt(a^2 + b^2)
 
-# friends = 0
-#
-# friends = friends + 1
-# friends = friends - 2
-# friends = friends * 4
-# friends = friends / 2
-# friends = friends ** 2
-# remainder = friends % 2
-#
-# print(friends)
-# print(remainder)
+# Asks user for diameter of circle
+diameter = float(input("What is the diameter of the circle: "))
+side1 = float(input("What is side1: "))
+side2 = float(input("What is side2: "))
 
-x = 9
-# y = -4
-# z = 5
+# Circumference calculation
+radius = diameter/2
+circumference = 2 * math.pi * radius
 
-# rounds to nearest whole number
-# result = round(x)
-# result = abs(y)
-# result = pow(z, 3)
+#area calculation
+area = math.pi * pow(radius,2)
 
-# result = max(x,y,z)
-#
-# print(result)
+#hypotenuse calculation
+hypotenuse = math.sqrt(pow(side1,2) + pow(side2, 2))
 
-# math.ceil function - rounds up
-# math.floor function - rounds down
-
-
-print(math.pi) # prints out pi = 3.141592653589793
-print(math.e) # prints out e = 2.718281828459045
-
-print(int(math.sqrt(x)))
+# Notifies user of calculation and rounds to 2 decimal places.
+print(f"1. The circumference is {round(circumference, 2)} cm.")
+print(f"2. The area of the circle is {round(area,2)} cm^2.")
+print(f"3. The hypotenuse of the right triangle is {round(hypotenuse,2)} cm")
