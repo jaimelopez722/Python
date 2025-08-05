9fd4c2b24ec2585fac1954e81186b9ce0be87559
Lopez <Jaime.Lopez@VPGSensors.com>
2025-08-05 09:50:37 -0700

    If elif else statement basics

--- PyCharmProjects/main.py
+++ PyCharmProjects/main.py
@@ -4,9 +4,11 @@
 
 age = int(input("Enter your age: "))
 
-if age >= 18:
+if age >= 100:
+    print("You are too old to sign up")
+elif age >= 18:
     print("You are now signed up")
 elif age < 0:
-    print
+    print("You haven't been born yet!")
 else:
     print("You must be 18+ to sign up")
