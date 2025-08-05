1bd6bc635cd97db4ed2d51e58ac4d059526f807e
Lopez <Jaime.Lopez@VPGSensors.com>
2025-08-05 10:11:13 -0700

    More if/else with some boolean

--- PyCharmProjects/main.py
+++ PyCharmProjects/main.py
@@ -1,14 +1,19 @@
-# if = Do some code only IF some condition is True
-#   Else do something else
+for_sale = True
+response = input("Would you like food? (Y/N): ").upper()
+name = input("Enter your name: ")
 
-
-age = int(input("Enter your age: "))
-
-if age >= 100:
-    print("You are too old to sign up")
-elif age >= 18:
-    print("You are now signed up")
-elif age < 0:
-    print("You haven't been born yet!")
+if response == "Y":
+    print("Have some food!")
 else:
-    print("You must be 18+ to sign up")
+    print("No food for you")
+
+
+if name == "":
+    print("You did not type in your name!!!!!!!!!!!")
+else:
+    print(f"Hello {name}!")
+
+if for_sale:
+    print("This item is for sale")
+else:
+    print("This item is not for sale")
