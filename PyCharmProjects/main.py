949468efddb89c6724e1d021cdb3829d5839fd59
Lopez <Jaime.Lopez@VPGSensors.com>
2025-08-07 10:56:20 -0700

    Calculator with if statements

--- PyCharmProjects/main.py
+++ PyCharmProjects/main.py
@@ -1,19 +1,25 @@
-for_sale = True
-response = input("Would you like food? (Y/N): ").upper()
-name = input("Enter your name: ")
+# Python calculator
 
-if response == "Y":
-    print("Have some food!")
+# asks user for operation to perform
+operator = input("Enter an operator (+ - * /) : ")
+
+# asks user for 2 numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+
+# goes through logic of what user chose
+if operator == '+':
+    result = num1 + num2
+    print(round(result,2))
+elif operator == '-':
+    result = num1 - num2
+    print(abs(result))
+elif operator == '*':
+    result = num1 * num2
+    print(round(result,2))
+elif operator == '/':
+    result = num1 / num2
+    print(round(result,2))
 else:
-    print("No food for you")
-
-
-if name == "":
-    print("You did not type in your name!!!!!!!!!!!")
-else:
-    print(f"Hello {name}!")
-
-if for_sale:
-    print("This item is for sale")
-else:
-    print("This item is not for sale")
+    print(f"{operator} is not a correct operator.")
