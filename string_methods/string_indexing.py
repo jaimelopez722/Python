87b683a6327d28c8dba305ca55b3ec162f6b5b48
Jaime Lopez <afjaimelopez@gmail.com>
2025-12-16 10:49:45 -0800

    string indexing

--- string_methods/string_indexing.py
+++ string_methods/string_indexing.py
@@ -1,5 +1,9 @@
 # indexing = accessing elements of a sequence using [] (indexing operator)
-#            [start : end : step]
+#            [start : end(exclusive: not including) : step]
 
 credit_number = "1234-5678-9012-4567"
 
+print(credit_number[4])
+print(credit_number[0:4])
+print(credit_number[5:9])
+print(credit_number[0::2])
